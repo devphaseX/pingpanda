184cbc122b789e2d139a40a9455ba16af0c5b1f7
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-01-05 07:22:44 +0100

    add navbar and heroes

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,67 @@
+import { MaxWidthWrapper } from "@/components/max_width_wrapper";
+import { Heading } from "@/components/heading";
+import { CheckIcon } from "lucide-react";
+import { ShinyButton } from "@/components/shiny_button";
+
+export default async function Home() {
+  return (
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper>
+          <div
+            className="relative mx-auto text-center flex
+            flex-col items-center gap-10"
+          >
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights</span>
+                <br />
+                <span
+                  className="relative bg-gradient-to-r from-brand-700 to-brand-800
+                    text-transparent bg-clip-text
+                  "
+                >
+                  Delivered to your Discord
+                </span>
+              </Heading>
+            </div>
+            <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+              PingPanda is the easiest way to monitor your saas. Get instant
+              notifications for{" "}
+              <span className="font-semibold text-gray-700">
+                sales, new users, or any other events
+              </span>{" "}
+              sent directly to your Discords.
+            </p>
+
+            <ul
+              className="space-y-2 text-base/7  text-gray-600 text-left
+              flex flex-col items-start"
+            >
+              {[
+                "Real-time Discord alerts for critical events",
+                "Buy one, use forever",
+                "Track sales, new users, or any other events",
+              ].map((item, i) => (
+                <li key={i} className="flex gap-1.5 items-center text-left">
+                  <CheckIcon className="size-5 shrink-0 text-brand-700" />
+                  {item}
+                </li>
+              ))}
+            </ul>
+
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base
+                shadow-lg  transition-shadow duration-300 hover:shadow-xl "
+              >
+                Start for Free Today
+              </ShinyButton>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
+  );
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import { MaxWidthWrapper } from "./max_width_wrapper";
+import { SignOutButton } from "@clerk/nextjs";
+import { Button, buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Navbar = () => {
+  const user = false;
+  return (
+    <nav
+      className="sticky z-[100px] h-16 inset-x-0 top-0 w-full border-b
+      border-gray-200 bg-white/80 backdrop-blur-lg transition-all   "
+    >
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-10 font-semibold">
+            Ping<span className="text-brand-700">Panda</span>
+          </Link>
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
+              <>
+                <SignOutButton>
+                  <Button size="sm" variant="ghost">
+                    Sign out
+                  </Button>
+                </SignOutButton>
+                <Button asChild size="sm" className="flex items-center gap-1">
+                  <Link href="/dashboard">
+                    Dashboard <ArrowRight className="ml-1.5 size-4" />
+                  </Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  asChild
+                  size="sm"
+                  className="flex items-center gap-1"
+                  variant="ghost"
+                >
+                  <Link href="/prices">Pricing</Link>
+                </Button>
+                <Button asChild size="sm" variant="ghost">
+                  <Link href="/sign-in">Sign in</Link>
+                </Button>
+
+                <div className="h-8 w-px bg-gray-200" />
+                <Button asChild size="sm" className="flex items-center gap-1.5">
+                  <Link href="/sign-in">
+                    Sign up <ArrowRight className="ml-2 size-4" />
+                  </Link>
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
