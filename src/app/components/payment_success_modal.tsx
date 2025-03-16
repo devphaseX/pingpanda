9bb8d5d40e2089ddf5db08c9e6d826ee2face791
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-03-16 12:45:26 +0100

    fix some bugs with payment success modal

--- src/app/components/payment_success_modal.tsx
+++ src/app/components/payment_success_modal.tsx
@@ -20,17 +20,19 @@ export const PaymentSuccessModal = () => {
     router.push("/dashboard");
   };
 
-  const isPaymentCompleted = true;
+  const isPaymentCompleted = plan === Plan.PRO;
 
   return (
     <Modal
       showModal={isOpen}
       setShowModal={setIsOpen}
       onClose={handleClose}
-      className="px-6 pt-6 h-full"
+      className="px-6 pt-6" // REMOVE h-full here
       preventDefaultClose={!isPaymentCompleted}
     >
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center h-full">
+        {" "}
+        {/*Add h-full to the parent container*/}
         {isPending || !isPaymentCompleted ? (
           <div className="flex flex-col items-center justify-center h-64">
             <LoadingSpinner className="mb-4" />
@@ -44,29 +46,29 @@ export const PaymentSuccessModal = () => {
           </div>
         ) : (
           <>
-            <div className="relative aspect-video border border-gray-200 w-full overflow-hidden rounded-lg bg-gray-50 h-full">
+            <div className="relative aspect-video border border-gray-200 w-full overflow-hidden rounded-lg bg-gray-50">
               <img
                 src="/brand-asset-heart.png"
-                className="h-full w-full object-cover"
+                className="object-cover" //removed h-full and w-full here.
                 alt="Payment Success"
               />
+            </div>{" "}
+            {/*Move div to wrap only img*/}
+            <div className="mt-6 flex flex-col items-center gap-1 text-center">
+              <p className="text-lg/7 tracking-tight font-medium text-pretty">
+                Upgrade Successful!
+              </p>
 
-              <div className="mt-6 flex flex-col items-center gap-1 text-center">
-                <p className="text-lg/7 tracking-tight font-medium text-pretty">
-                  Upgrade Successful!
-                </p>
+              <p className="text-gray-600 text-sm/6 text-pretty">
+                Thank you for upgrading to Pro and supporting PingPanda. Your
+                account has been upgraded successfully.
+              </p>
 
-                <p className="text-gray-600 text-sm/6 text-pretty">
-                  Thank your for upgrading to Pro and supporting PingPanda. Your
-                  account has been upgraded successfully.
-                </p>
-
-                <div className="mt-8 w-full">
-                  <Button onClick={handleClose} className="h-12 w-full">
-                    <CheckIcon className="mr-2 size-5" />
-                    Go to Dashboard
-                  </Button>
-                </div>
+              <div className="mt-8 w-full">
+                <Button onClick={handleClose} className="h-12 w-full">
+                  <CheckIcon className="mr-2 size-5" />
+                  Go to Dashboard
+                </Button>
               </div>
             </div>
           </>
