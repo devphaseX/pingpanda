cd07c59a1cbe69c08ca4cbf570195f77b2bc149a
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-03-16 14:43:44 +0100

    fix issue with user account not sync

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,25 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { Navbar } from "../components/navbar";
+import { getUserByExternalId } from "@/features/users/server/user.service";
+import { redirect } from "next/navigation";
 
 interface LandingPageLayoutProps {
   children: React.ReactNode;
 }
 
-export default function LandingPageLayout({
+export default async function LandingPageLayout({
   children,
 }: LandingPageLayoutProps) {
+  const auth = await currentUser();
+  const user = auth ? getUserByExternalId(auth.id) : null;
+
+  if (auth) {
+    if (user) {
+      return redirect("/welcome");
+    }
+    return redirect("/dashboard");
+  }
+
   return (
     <>
       <Navbar />
